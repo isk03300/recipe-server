0dfb553e9f763ec8de4a276b9bcc604c011243fd
isk03300 <isk03300@naver.com>
2023-12-05 17:26:08 +0900

    add

---
+++ mysql_connection.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+
+# 파이썬으로 mysql에 접속할 수 있는 함수.
+def get_connection() :
+    connection = mysql.connector.connect(
+        host = 'dbsample.cmuxpoe5idov.ap-northeast-2.rds.amazonaws.com',
+        database = 'recipe_db',
+        user = 'recipe_db_user',
+        password = '1234'
+    )
+    return connection
